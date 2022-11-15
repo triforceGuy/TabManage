701dfbbab4a6c9949faf75e62f785c7c649c8c1a
Eri Midorikawa <etm43@txstate.edu>
2022-11-14 23:41:24 -0600

    Modified testlib

--- TabManagerExtension/lib/testlib/TabManager.test.js
+++ TabManagerExtension/lib/testlib/TabManager.test.js
@@ -4,10 +4,10 @@ import Button from './Button';
 
 describe('Text Tests', ()=>{
     test('darkText Test', ()=>{
-        expect(darkText()).toBe("Dark mode inverts the layout - better on the eyes. Default : off");
+        expect(darkText()).toHaveProperty('bottomText', "SDark mode inverts the layout - better on the eyes. Default : off");
     });
 
     test('spaceText Test', ()=>{
-        expect(spaceText()).toBe("Space mode, for when you want a cool theme. Default : off");
+        expect(spaceText()).toHaveProperty('bottomText', "Space mode, for when you want a cool theme. Default : off");
     });
 });
